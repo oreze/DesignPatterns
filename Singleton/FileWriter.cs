d3e8f39db3e18e1037c91f564fdaebbb05404eec
oreze <kunas1@o2.pl>
2021-05-14 19:41:37 +0200

    Add 'Singleton' pattern

---
+++ Singleton/FileWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1.Singleton
+{
+    class FileWriter
+    {
+        private static FileWriter instance = null;
+
+        private FileWriter()
+        {
+        }
+
+        public static FileWriter GetInstance()
+        {
+            if (instance == null)
+                instance = new FileWriter();
+            return instance;
+
+        }
+
+        public void SaveToPath(string data, string path)
+        {
+            Console.WriteLine($"Data saved to {path}");
+        }
+    }
+}
---
+++ Singleton/OpenWeatherHttpClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Singleton
+{
+    public class OpenWeatherHttpClient
+    {
+        private HttpClient _client { get; }
+        private FileWriter _writer { get; };
+        
+        public OpenWeatherHttpClient(HttpClient client,
+            FileWriter writer)
+        {
+            client.BaseAddress = new Uri("http://api.openweathermap.org");
+            client.DefaultRequestHeaders.Add("User-Agent", "Asp.Net-Forecaster");
+
+            _client = client;
+            _writer = writer;
+        }
+        
+        public async Task GetLocationWeather(string location)
+        {
+            var requestUri = "/data/2.5/" +
+                             "weather?q=" + location +
+                             "&appid=" + "api_key";
+            
+            var response = await _client.GetAsync(requestUri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                var deserialized = await JsonSerializer.DeserializeAsync<CityWeather>(responseStream);
+                _writer.SaveTopath(deserialized.ToString());
+            }
+        }
+    }
+}
